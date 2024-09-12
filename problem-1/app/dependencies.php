bccc0bcb0c31e299f9c0aca914f72167947843bf
Thijs Windels <thijs.windels@dx-solutions.be>
2024-09-12 07:50:17 +0200

    Implement and enable Symfony's message bus
    Added Symfony messenger to be able to use the message bus. This allows
    us to use commands and command handlers.

--- problem-1/app/dependencies.php
+++ problem-1/app/dependencies.php
@@ -2,6 +2,10 @@
 
 declare(strict_types=1);
 
+use App\Application\Command\CreateOrderFromRequestDataCommand;
+use App\Application\Command\CreateOrderFromRequestDataCommandHandler;
+use App\Application\Command\CreateOrderItemFromRequestDataCommand;
+use App\Application\Command\CreateOrderItemFromRequestDataCommandHandler;
 use App\Application\Settings\SettingsInterface;
 use DI\ContainerBuilder;
 use Monolog\Handler\StreamHandler;
@@ -9,6 +13,10 @@ use Monolog\Logger;
 use Monolog\Processor\UidProcessor;
 use Psr\Container\ContainerInterface;
 use Psr\Log\LoggerInterface;
+use Symfony\Component\Messenger\Handler\HandlersLocator;
+use Symfony\Component\Messenger\MessageBus;
+use Symfony\Component\Messenger\MessageBusInterface;
+use Symfony\Component\Messenger\Middleware\HandleMessageMiddleware;
 
 return function (ContainerBuilder $containerBuilder) {
     $containerBuilder->addDefinitions([
@@ -25,6 +33,24 @@ return function (ContainerBuilder $containerBuilder) {
             $logger->pushHandler($handler);
 
             return $logger;
+        },
+        MessageBusInterface::class => function (ContainerInterface $c) {
+            $containerHandlerLocator = new HandlersLocator([
+                CreateOrderFromRequestDataCommand::class => [
+                    fn (CreateOrderFromRequestDataCommand $command) =>
+                        $c->get(CreateOrderFromRequestDataCommandHandler::class)($command)
+                ],
+                CreateOrderItemFromRequestDataCommand::class => [
+                    fn (CreateOrderItemFromRequestDataCommand $command) =>
+                        $c->get(CreateOrderItemFromRequestDataCommandHandler::class)($command)
+                ],
+            ]);
+
+            $middlewares = [
+                new HandleMessageMiddleware($containerHandlerLocator),
+            ];
+
+            return new MessageBus($middlewares);
         },
     ]);
 };
