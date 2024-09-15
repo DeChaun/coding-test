5142841145ff342f43c2b19160c7460ce9087cb8
Thijs Windels <thijs.windels@dx-solutions.be>
2024-09-15 14:57:24 +0200

    Create a response to send back to the client
    Sent the response back + added documentation about how to do it.

--- problem-1/src/Infrastructure/Controller/Order/CalculateDiscountController.php
+++ problem-1/src/Infrastructure/Controller/Order/CalculateDiscountController.php
@@ -7,6 +7,7 @@ namespace App\Infrastructure\Controller\Order;
 use App\Application\Command\Discount\ComputeDiscount;
 use App\Infrastructure\Factory\OrderFactory;
 use Psr\Http\Message\ResponseInterface;
+use Psr\Log\LoggerInterface;
 use Slim\Psr7\Request;
 use Slim\Psr7\Response;
 use Symfony\Component\Messenger\HandleTrait;
@@ -18,6 +19,7 @@ final class CalculateDiscountController
 
     public function __construct(
         private readonly OrderFactory $orderFactory,
+        private readonly LoggerInterface $logger,
         MessageBusInterface $queryBus,
     ) {
         $this->messageBus = $queryBus;
@@ -32,10 +34,9 @@ final class CalculateDiscountController
         assert(is_array($body));
 
         $order = $this->orderFactory->fromApiData($body);
-
         $discountOrder = $this->handle(new ComputeDiscount($order));
 
-        $response->getBody()->write('test response');
+        $response->getBody()->write(json_encode($discountOrder->toArray()));
         return $response->withHeader('Content-Type', 'application/json');
     }
 }
